ffb584c101dacfd7915fe16905edcbb93dd97538
wxxb789 <wxxb789@outlook.com>
2025-03-11 00:07:50 +0800

    fix path issue.

--- test/services/aktools/UsEmData.test.ts
+++ test/services/aktools/UsEmData.test.ts
@@ -1,23 +1,23 @@
-import UsEmDataService from '@/services/aktools/UsEmData';
-import { beforeAll, describe, expect, it } from 'vitest';
+import UsEmDataService from "@/services/aktools/UsEmData";
+import { beforeAll, describe, expect, it } from "vitest";
 
-describe('UsEmDataService', () => {
+describe("UsEmDataService", () => {
   beforeAll(async () => {
     await UsEmDataService.init();
   });
 
   it('should return "105.QQQ" for query "QQQ"', () => {
-    const result = UsEmDataService.getValue('QQQ');
-    expect(result).toBe('105.QQQ');
+    const result = UsEmDataService.getValue("QQQ");
+    expect(result).toBe("105.QQQ");
   });
 
   it('should return "107.SPY" for query "SPY"', () => {
-    const result = UsEmDataService.getValue('SPY');
-    expect(result).toBe('107.SPY');
+    const result = UsEmDataService.getValue("SPY");
+    expect(result).toBe("107.SPY");
   });
 
   it('should return "105.MSFT" for query "MSFT"', () => {
-    const result = UsEmDataService.getValue('MSFT');
-    expect(result).toBe('105.MSFT');
+    const result = UsEmDataService.getValue("MSFT");
+    expect(result).toBe("105.MSFT");
   });
 });
